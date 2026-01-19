378ac36370ada434f53a0060149d251450aad374
Christina Bakatsi <bakatsi.chr@namanet.gr>
2026-01-19 14:41:42 +0200

    Describe the change

--- bus_lines_stop_id.py
+++ bus_lines_stop_id.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).parent
 # --- Excel data (Nov 2025) ---
 DATA_XLSX_FILES = [
     BASE_DIR / "data" / "Per_Day_Per_Stop_Per_Line_November_2025_excel.csv",
-   #BASE_DIR / "data" / "Per_Day_Per_Stop_Per_Line_November_2025_b_codes.xlsx",
+    BASE_DIR / "data" / "Per_Day_Per_Stop_Per_Line_November_2025_excel_b.csv",
 ]
 
 
@@ -360,6 +360,7 @@ def load_excel_many(paths: list[Union[str, Path]]) -> pd.DataFrame:
 
 
     return df_all
+    
 
 
 @st.cache_resource(show_spinner=False)
